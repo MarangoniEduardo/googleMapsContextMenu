17ae84b97e5ae157bf29c0aa70bec4ebcbf06aff
Eduardo Marangoni <marangoni_eduardo@icloud.com>
2019-09-17 02:59:48 -0300

    It's done! Fantastic! Off to sleep I go.

--- googleMapsContextMenu.js
+++ googleMapsContextMenu.js
@@ -1,11 +1,15 @@
 class googleMapsContextMenu {
-    constructor(map, markers, layout, appendDefault, themeColor) {
+    constructor(map, layout, markers, themeColor, appendDefault) {
         this._map = map;
-        this._appendDefault = appendDefault || true;
-        this.markers = markers;
-        this.layout = layout || this.getDefaultLayout();
+        this._appendDefault = appendDefault ? appendDefault : true;
+        this._themeColor = themeColor ? themeColor : `rgba(16, 150, 145)`;
+        this.markers = markers ? markers : [];
+        this.layout = layout ? layout : this.getDefaultLayout();
         this.currentInstance = [];
+        this.divisionContainer = [];
         this.eventCoordinate = {lat: null, lng: null};
+        this.clickedMarker = {};
+        this.event = {};
     }
 
     get appendDefault() {
@@ -16,6 +20,10 @@ class googleMapsContextMenu {
         return this._map;
     }
 
+    get themeColor() {
+        return this._themeColor;
+    }
+
     init() {
         this.initEvents();
     }
@@ -24,69 +32,197 @@ class googleMapsContextMenu {
         let container = document.createElement('div');
         container.setAttribute(`id`, `googleMapsContextMenu-container`);
         container.style.position = `absolute`;
+        container.style.backgroundColor = `white`;
         container.style.padding = `5px`;
+        container.style.borderRadius = `10px`;
 
         container.style.width = `200px`;
-        container.style.height = `${(this.layout.length * 45) + 15}px`;
+        container.style.height = `${(this.layout.length * 35) + 10}px`;
 
-        container.style.left = event.x;
-        container.style.bottom = event.y;
-
-        this.layout.forEach((division) => {
-           let divisionReference = document.createElement('div');
-           divisionReference.style.width = `100%`;
-           divisionReference.style.padding = `5px`;
-           divisionReference.innerHTML = `<span><img src="${division.icon}" alt="Context Menu Icon" height="25px"/>${division.label}</span>`;
-
-           this.addEvent(divisionReference, division.event);
-
-           container.appendChild(divisionReference);
-        });
+        container.style.left = `${event.xa.x}px`;
+        container.style.top = `${event.xa.y}px`;
 
         this.currentInstance = container;
 
+        this.event = event;
+        this.eventCoordinate.lat = event.latLng.lat();
+        this.eventCoordinate.lng = event.latLng.lng();
+
+        this.buildDivision();
         document.body.appendChild(this.currentInstance);
+
+        this.defaultUiEvents();
     }
 
-    add() {
+    buildDivision() {
+        if(this.divisionContainer.parentNode) {
+            this.divisionContainer.remove();
+        }
 
+        this.divisionContainer = document.createElement(`div`);
+
+        this.layout.forEach((division) => {
+            let divisionInstance = document.createElement('div');
+            divisionInstance.setAttribute(`id`, division.id);
+            divisionInstance.setAttribute(`class`, `googleMapsContextMenu-division`);
+            divisionInstance.style.width = `100%`;
+            divisionInstance.style.height = `35px`;
+            divisionInstance.style.padding = `5px`;
+            divisionInstance.style.borderRadius = `10px`;
+            divisionInstance.style.cursor = `pointer`;
+
+            if(division.icon) {
+                let img = document.createElement(`img`);
+                img.setAttribute(`alt`, `Context Menu Icon`);
+                img.setAttribute(`src`, division.icon);
+                img.style.height = `25px`;
+                img.style.width = `25px`;
+                divisionInstance.appendChild(img);
+            }
+
+            let spanTitle = document.createElement(`span`);
+            spanTitle.innerText = division.label;
+            divisionInstance.appendChild(spanTitle);
+
+            this.addEvent(divisionInstance, division.event);
+
+            this.divisionContainer.appendChild(divisionInstance);
+        });
+
+        this.currentInstance.appendChild(this.divisionContainer);
     }
 
-    remove() {
+    add(menuObject) {
+        menuObject.temp = true;
+        this.layout.push(menuObject);
+        this.currentInstance.style.height = `${(this.layout.length * 35) + 10}px`;
+        this.buildDivision();
 
+        this.defaultUiEvents();
+    }
+
+    remove(id) {
+        this.layout.forEach((elem, index) => {
+            if(elem.id === id) {
+                delete this.layout[index];
+            }
+        });
+
+        this.layout = this.layout.filter(() => {return true;});
+
+        this.currentInstance.style.height = `${(this.layout.length * 35) + 10}px`;
+
+        document.getElementById(id).remove();
+
+        this.defaultUiEvents();
+    }
+
+    removeAdded() {
+        this.layout.forEach((elem, index) => {
+            if(elem.temp) {
+                delete this.layout[index];
+            }
+        });
+
+        this.layout = this.layout.filter(() => {return true;});
+
+        this.defaultUiEvents();
     }
 
     initEvents() {
-        this.map.addListener(`rightclick`, () => {
-
+        this.map.addListener('rightclick', (event) => {
+            this.removeAdded();
+            this.destroy();
+            this.build(event);
         });
+
+        this.map.addListener(`click`, () => {
+            this.removeAdded();
+            this.destroy();
+        });
+
+        this.map.addListener('dragstart', () => {
+            this.removeAdded();
+            this.destroy();
+        });
+
+        this.map.addListener('bounds_changed', () => {
+            this.removeAdded();
+            this.destroy();
+        });
+
+        this.markerEvents();
     }
 
-    updateMarkers(markers) {
+    markerEvents() {
+        if(this.markers.length) {
+            let that = this;
 
+            this.markers.forEach((marker) => {
+                marker.addListener('rightclick', (event) => {
+                    that.clickedMarker = marker;
+                    this.destroy();
+                    this.build(event);
+                });
+            });
+        }
+    }
+
+    updateMarkers(markers, markerOptionsLayout) {
+        if(markerOptionsLayout) {
+            this.removeAdded();
+
+            markerOptionsLayout.forEach((obj) => {
+                this.add(obj)
+            });
+        }
+
+        this.markers = markers;
+        this.markerEvents();
     }
 
     getDefaultLayout() {
-        return [
-            {label: 'Zoom In', icon: '',
-                event: () => {
-                    this.map.setZoom(parseInt(this.map.getZoom()) + 1);
+        if(this.appendDefault) {
+            return [
+                {label: 'Zoom In', icon: '', id: 'googleMapsContextMenu-zoomIn',
+                    event: {
+                        'click': () => {
+                            this.map.setZoom(parseInt(this.map.getZoom()) + 1);
+                        }
+                    }
+                },
+                {
+                    label: 'Zoom Out', icon: '', id: 'googleMapsContextMenu-zoomOut',
+                    event: {
+                        'click': () => {
+                            this.map.setZoom(parseInt(this.map.getZoom()) - 1);
+                        }
+                    }
                 }
-            },
-            {label: 'Zoom Out', icon: '',
-                event: () => {
-                    this.map.setZoom(parseInt(this.map.getZoom()) - 1);
-                }
-            }
-        ];
+            ];
+        }
     }
 
     defaultUiEvents() {
+        let selector = document.querySelectorAll(`.googleMapsContextMenu-division`);
 
+        Array.from(selector).forEach((element) => {
+            element.addEventListener('mouseover', () => {
+                element.style.backgroundColor = this.themeColor;
+                element.style.color = `white`;
+            });
+
+            element.addEventListener('mouseout', () => {
+                element.style.backgroundColor = `white`;
+                element.style.color = `#6b6f82`;
+            });
+        });
     }
 
     destroy() {
-        this.currentInstance.remove();
+        if(this.currentInstance.parentNode) {
+            this.currentInstance.remove();
+        }
     }
 
     addEvent(element, event) {
